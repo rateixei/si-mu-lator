e17f2b0e12f57afc8f1101b02b2dd104e22e0054
Rafael Teixeira De Lima <rafael.teixeira.de.lima@cern.ch>
2022-04-08 01:51:57 -0700

    fix pitch size

--- nn/linefit.py
+++ nn/linefit.py
@@ -7,7 +7,7 @@ def get_mm_line_fit(hits, sig_keys):
     xs = hits[:, sig_keys.index('projX_at_middle_x')]
     unc_xs = 2*np.abs( hits[:, sig_keys.index('projX_at_middle_x')] - 
                       hits[:, sig_keys.index('projX_at_rightend_x')] )
-    unc_xs[unc_xs < 0.4] = 0.4
+    unc_xs[unc_xs < 0.45] = 0.45
     
     ## only save the ones with valid hits
     zs = zs[ hits[:, sig_keys.index('is_muon')] > -1 ]
@@ -20,14 +20,14 @@ def get_mm_line_fit(hits, sig_keys):
     _c3 = ( zs*xs/unc_xs**2 ).sum() #omega
     _c4 = ( 1./unc_xs**2 ).sum() #lambda
     _c5 = ( xs/unc_xs**2 ).sum() #rho
-    
+        
     ## calculate line parameters
     A = ( _c5*_c2 - _c3*_c4 ) / ( _c2**2 - _c1*_c4 )
     B = ( _c3 - A*_c1 ) / _c2
     
     ## calculate chi2 value
     Chi2 = ( ( A*zs + B - xs )**2 / unc_xs**2 ).sum()
-    
+        
     return [A, B, Chi2]
     
 
