927b5fdfe8efe3d44b0494b3c87d7cdaad16d5d2
rateixei <rafael.teixeira.de.lima@cern.ch>
2022-06-28 08:13:26 -0700

    fixes and improvements for training

--- algorithms/exec_train.sh
+++ algorithms/exec_train.sh
@@ -4,4 +4,4 @@ SIM="/gpfs/slac/atlas/fs1/d/rafaeltl/public/Muon/simulation/"
 DATA_LOC="${SIM}/20220628/atlas_mm_vmm_bkgr_1_TRAIN/*.h5"
 SING_IMG=/sdf/group/ml/software/images/slac-ml/20211101.0/slac-ml@20211101.0.sif
 
-singularity exec -B /sdf,/gpfs,/scratch ${SING_IMG} python train.py -f "${DATA_LOC}" -m "lstm"
+singularity exec -B /sdf,/gpfs,/scratch ${SING_IMG} python train.py -f "${DATA_LOC}" -m "gru" --batchnorm
